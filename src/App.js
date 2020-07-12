939e03d266fb09e5b23acc3e0df32411406d4e0d
pedroddomingues <pedroddomingues@gmail.com>
2020-07-12 13:47:51 -0300

    Desafio concluido com botao para apagar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import {
   SafeAreaView,
@@ -10,46 +10,82 @@ import {
   TouchableOpacity,
 } from "react-native";
 
+import api from './services/api'
+
 export default function App() {
+  const [ repositories, setRepositories ] = useState([])
+
+  useEffect(() => {
+    api.get('repositories').then(response => setRepositories(response.data))
+  }, [])
+
   async function handleLikeRepository(id) {
-    // Implement "Like Repository" functionality
+    const repoI = repositories.findIndex(repo => repo.id === id)
+
+    const response = await api.post(`repositories/${id}/like`)
+
+    repositories[repoI] = {...repositories[repoI], likes: response.data.likes}
+
+    setRepositories([...repositories])
+  }
+
+  async function handleDeleteRepository(id) {
+    const repoI = repositories.findIndex(repo => repo.id === id)
+
+    await api.delete(`repositories/${id}`)
+
+    repositories.splice(repoI,1)
+
+    setRepositories([...repositories])
   }
 
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
       <SafeAreaView style={styles.container}>
-        <View style={styles.repositoryContainer}>
-          <Text style={styles.repository}>Repository 1</Text>
+        <FlatList
+        data={repositories}
+        keyExtractor={repo => repo.id}
+        renderItem={({ item: repository }) => (
+          <View style={styles.repositoryContainer}>
+            <Text style={styles.repository}>{repository.title}</Text>
 
-          <View style={styles.techsContainer}>
-            <Text style={styles.tech}>
-              ReactJS
-            </Text>
-            <Text style={styles.tech}>
-              Node.js
-            </Text>
-          </View>
-
-          <View style={styles.likesContainer}>
-            <Text
-              style={styles.likeText}
-              // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-              testID={`repository-likes-1`}
-            >
-              3 curtidas
-            </Text>
-          </View>
-
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => handleLikeRepository(1)}
-            // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-            testID={`like-button-1`}
-          >
-            <Text style={styles.buttonText}>Curtir</Text>
-          </TouchableOpacity>
+            <View style={styles.techsContainer}>
+            {repository.techs.map(tech => (
+                  <Text key={tech} style={styles.tech}>
+                    {tech}
+                  </Text>
+            ))}
+            </View>
+            <View style={styles.likesContainer}>
+              <Text
+                style={styles.likeText}
+                // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
+                testID={`repository-likes-${repository.id}`}
+              >
+                {`${repository.likes} curtidas`}
+              </Text>
+            </View>
+            <View style={styles.buttons}>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => handleLikeRepository(repository.id)}
+                // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
+                testID={`like-button-${repository.id}`}
+                >
+                <Text style={styles.buttonLikeText}>Curtir</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => handleDeleteRepository(repository.id)}
+                >
+                <Text style={styles.buttonDeleteText}>Apagar</Text>
+              </TouchableOpacity>
+            </View>
         </View>
+        )}
+        >
+        </FlatList>
       </SafeAreaView>
     </>
   );
@@ -92,16 +128,27 @@ const styles = StyleSheet.create({
     fontSize: 14,
     fontWeight: "bold",
     marginRight: 10,
+  }, 
+  buttons: {
+    flexDirection: "row"
   },
   button: {
     marginTop: 10,
   },
-  buttonText: {
+  buttonLikeText: {
     fontSize: 14,
     fontWeight: "bold",
     marginRight: 10,
     color: "#fff",
-    backgroundColor: "#7159c1",
+    backgroundColor: "pink",
     padding: 15,
   },
+  buttonDeleteText: {
+    fontSize: 14,
+    fontWeight: "bold",
+    marginRight: 10,
+    color: "#fff",
+    backgroundColor: "red",
+    padding: 15,
+  }
 });
